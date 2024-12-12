29ae7e26997ff43eb843b32e29ce9b861fedc5b7
kyASse <akyasmuhamadzaidan59@gmail.com>
2024-12-12 23:38:30 +0700

    - Added MenuButton to DrawerContainer.jsx

--- src/screens/DrawerContainer/DrawerContainer.jsx
+++ src/screens/DrawerContainer/DrawerContainer.jsx
@@ -1,14 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View } from 'react-native'
+import PropTypes from "prop-types"
+import MenuButton from "../../components/MenuButton/MenuButton"
 import styles from './DrawerContainer.style'
 
-const DrawerContainer = () => {
+export default function DrawerContainer(props) {
+    const { navigation } = props;
     return (
-        <View>
-            <Text>DrawerContainer</Text>
+        <View style={styles.content}>
+            <View style={styles.container}>
+                <MenuButton
+                    title="HOME"
+                    source={require("../../../assets/icons/home.png")}
+                    onPress={() => {
+                        navigation.navigate("Main", { screen: "Home"});
+                        navigation.closeDrawer();
+                    }}
+                />
+                <MenuButton
+                    title="CATEGORIES"
+                    source={require("../../../assets/icons/category.png")}
+                    onPress={() => {
+                        navigation.navigate("Main", { screen: "Categories"});
+                        navigation.closeDrawer();
+                    }}
+                />
+                <MenuButton
+                    title="SEARCH"
+                    source={require("../../../assets/icons/search.png")}
+                    onPress={() => {
+                        navigation.navigate("Main", { screen: "Search"});
+                        navigation.closeDrawer();
+                    }}
+                />
+            </View>
         </View>
-    )
+    );
 }
 
-export default DrawerContainer
+DrawerContainer.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+        closeDrawer: PropTypes.func.isRequired,
+    }),
+};
