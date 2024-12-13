fd7f5b572963ff981b5ae00d3865d9e4aa4f2c6a
kyASse <akyasmuhamadzaidan59@gmail.com>
2024-12-13 15:24:08 +0700

    - Added HomeScreen.jsx

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React, { useLayoutEffect } from 'react';
+import { FlatList, View, Text, TouchableHighlight, Image } from 'react-native';
 import styles from './HomeScreen.style'
+// import { recipes } from '../../data/dataArrays';
+import MenuImage from "../../components/MenuImage/MenuImage";
+// import { getCategoriesName } from "../../data/MockDataAPI";
 
-const HomeScreen = () => {
+export default function HomeScreen(props) {
+    const { navigation } = props;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <MenuImage
+                    onPress={() => {
+                        navigation.openDrawer();
+                    }}
+                />
+            ),
+            headerRight: () => <View />,
+        })
+    }, []);
+
+    const onPressRecipe = (item) => {
+        navigation.navigate("Recipe", { item });
+    };
+
+    const renderRecipes = ({ item }) => (
+        <TouchableHighlight underlayColor="rgba(73, 182, 77, 0.9)" onPress={() => onPressRecipe(item)}>
+            <View style={styles.container}>
+                <Image style={styles.photo} source={{ uri: item.photo_url }} />
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.category}>{getCategoriesName(item.categoryId)}</Text>
+            </View>
+        </TouchableHighlight>
+    );
+
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <FlatList vertical showsVerticalScrollIndicator={false} numColumns={2} data={recipes} renderItem={renderRecipes} keyExtractor={(item) => `${item.recipeId}`} />
         </View>
-    )
+    );
 }
-
-export default HomeScreen
